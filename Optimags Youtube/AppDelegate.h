7da2212035efab590b9e4f3b41b8b7cd60d2c7e5
Anvesh <anvesh@Anveshs-MacBook-Pro.local>
2017-10-05 23:13:42 -0500

    Initial Commit

---
+++ Optimags Youtube/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Optimags Youtube
+//
+//  Created by Anvesh on 10/5/17.
+//  Copyright © 2017 Anvesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Optimags Youtube/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Optimags Youtube
+//
+//  Created by Anvesh on 10/5/17.
+//  Copyright © 2017 Anvesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
